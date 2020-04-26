73288c02bcdfb37f94019befed527190f9596cd4
mileto1975 <mileto.di.marco@howest.be>
2020-04-26 14:43:00 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//declare global vars here
+ window.addEventListener('load',Initialize);
+
+function Initialize()
+{
+   //bind HTML elements
+   
+   //add eventlisteners
+   
+}
+
+/**FUNCTIONS here or in seperate functions file */
+
+
+
+
+
+
+
+
