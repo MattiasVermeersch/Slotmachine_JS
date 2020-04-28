93209fb2fe515afd1da279c20ad7d3be26b4b2ff
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2020-04-28 18:40:03 +0200

    Add code start to the app

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
 "use strict";
 
 //declare global vars here
- window.addEventListener('load',Initialize);
+var btnRoll, btnStopRoll, btnReset;
+var images;
+var spanScorePerSpel, spanScoreHistoriek, spanAantalRolls;
+
+window.addEventListener('load',Initialize);
 
 function Initialize()
 {
    //bind HTML elements
-   
+   BindHtml();
    //add eventlisteners
    
 }
 
 /**FUNCTIONS here or in separate functions file */
+function BindHtml() {
+   images = document.querySelectorAll("img");
+   spanScorePerSpel = document.querySelector("#scorePerSpel");
+   spanScoreHistoriek = document.querySelector("#scoreHistoriek");
+   spanAantalRolls = document.querySelector("#aantalRolls");
+   btnRoll = document.querySelector("#btnRoll");
+   btnStopRoll = document.querySelector("#btnStopRoll");
+   btnReset = document.querySelector("#btnReset");
+}
 
+function AddEvents() {
+   btnRoll.addEventListener('click', StartSlotMachine);
+   btnStopRoll.addEventListener('click', StopSlotMachine);
+   btnReset.addEventListener('click', ResetSlotMachine);
+}
 
+function StartSlotMachine() {
 
+}
 
+function StopSlotMachine() {
 
+}
 
-
-
+function ResetSlotMachine() {
+   
+}
